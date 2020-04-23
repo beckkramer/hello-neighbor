fff5d6fc3c77d692360c26a4e05171a9fdea5ce5
beckkramer <kramer.beck@gmail.com>
2020-04-23 08:20:30 -0500

    Replaces zipcode data, attribution, and responses

--- server/api/location.js
+++ server/api/location.js
@@ -1,22 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const allZipcodes = require('../data/zipcode-data.json');
+
+// Data from https://public.opendatasoft.com/explore/dataset/us-zip-code-latitude-and-longitude
+// Dataset Identifier: us-zip-code-latitude-and-longitude
+// Record ID:: 6a0a9c66f8e0292a54c9f023c93732f1b41d8943
+const allZipcodes = require('../data/us-zip-code-latitude-and-longitude.json');
 
 /* GET data by zipcode match */
 router.get('/:zip', function(request, response, next) {
 
-  let zipcodeData = {};
+  let zipcodeData;
 
   if (request.params.zip) {
 
     const zipcode = request.params.zip;
 
     zipcodeData = allZipcodes.filter((entry) => {
-      return String(entry.Zipcode).includes(zipcode);
+      return String(entry.fields.zip).includes(zipcode);
     });
   }
 
-  response.status(200).json(zipcodeData);
+  if (zipcodeData) {
+    response.status(200).json(zipcodeData[0].fields);
+  } else {
+    response.status(400).send('Zipcode not found.');
+  }
 });
 
 module.exports = router;
