908637a006ba9515d93964b525b2002edd6714bf
beckkramer <kramer.beck@gmail.com>
2020-04-24 07:44:57 -0500

    Adds heroku and built client accommodations

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ const port = process.env.PORT || 5000;
 app.use('/api/location', location);
 app.use('/api/ebird', ebird);
 
-// Run the app by serving the static files in the dist directory
-app.use(express.static(__dirname + '/client/dist'));
+// Run the app by serving the static files in the client directory
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
